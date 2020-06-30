e5c23a982bae34d4b492158c38c0fa481cbd0399
Welder Rodrigues <welder_15@hotmail.com>
2020-06-30 01:01:11 -0300

    Layout
    Feito o layout da aplicação

--- App.js
+++ App.js
@@ -1,12 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+
+const buttons = [
+  ["LIMPAR", "VOLTAR"],
+  [7, 8, 9, "/"],
+  [4, 5, 6, "x"],
+  [1, 2, 3, "-"],
+  [0, ".", "=", "+"],
+];
 
 export default function App() {
+  function handleOnPress() {}
+
+  function renderButtons() {
+    let layouts = buttons.map((buttonRows, index) => {
+      let rowItem = buttonRows.map((buttonItems, buttonIndex) => {
+        return (
+          <TouchableOpacity
+            key={"btn" + buttonIndex}
+            value={buttonItems}
+            onPress={() => handleOnPress(buttonItems)}
+            style={styles.numbersContainer}
+          >
+            <Text style={styles.numberText}>{buttonItems}</Text>
+          </TouchableOpacity>
+        );
+      });
+
+      return (
+        <View key={"row" + index} style={styles.inputRow}>
+          {rowItem}
+        </View>
+      );
+    });
+
+    return layouts;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.resultContainer}>
+        <Text style={styles.result}>0</Text>
+      </View>
+
+      <View style={styles.inputContainer}>{renderButtons()}</View>
     </View>
   );
 }
@@ -14,8 +51,37 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  resultContainer: {
+    flex: 2,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+  },
+  inputContainer: {
+    flex: 8,
+    backgroundColor: "#7159c1",
+  },
+  result: {
+    color: "#000",
+    fontSize: 80,
+    fontWeight: "bold",
+    padding: 20,
+    textAlign: "right",
+  },
+  inputRow: {
+    flex: 1,
+    flexDirection: "row",
+  },
+
+  numbersContainer: {
+    flex: 1,
+    margin: 1,
+    backgroundColor: "rgba(255,255,255,0.1)",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  numberText: {
+    color: "#fff",
+    fontSize: 26,
   },
 });
